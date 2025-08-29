f17374c7eb4fa5bbc92a9d119ae0a1376394e6eb
bhoomika chandani <bhoomikachandani685@gmail.com>
2025-08-29 18:59:33 +0530

    My new changes

--- script.js
+++ script.js
@@ -14,3 +14,21 @@ if (close) {
     })
 }
 
+
+        var MainImg = document.getElementById("MainImg");
+        var smallimg = document.getElementById("small-img");
+
+        smallimg[0].onclick = function() {
+            MainImg.src = smallimg[0].src;
+        }
+        smallimg[1].onclick = function() {
+            MainImg.src = smallimg[1].src;
+        }
+        smallimg[2].onclick = function() {
+            MainImg.src = smallimg[2].src;
+        }
+        smallimg[3].onclick = function() {
+            MainImg.src = smallimg[3].src;
+        }
+  
+
